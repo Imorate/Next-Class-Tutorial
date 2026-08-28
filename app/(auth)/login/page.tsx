1d9cdb8f2d2fde9dfdf9ba79c69fdf374237c087
Amir Mohammad Hl <dev.imorate@gmail.com>
2026-08-28 03:40:26 +0330

    refactor: Add route group for auth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,15 @@
-import { SignupForm } from "@/app/signup/_components/signup-form";
 import { Metadata } from "next";
+import { LoginForm } from "./_components/login-form";
 
 export const metadata: Metadata = {
-  title: "ثبت نام",
+  title: "ورود",
 };
 
-export default function SignupPage() {
+export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-md">
-        <SignupForm />
+        <LoginForm />
       </div>
     </div>
   );
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,15 +1,15 @@
-import { LoginForm } from "@/app/login/_components/login-form";
 import { Metadata } from "next";
+import { SignupForm } from "./_components/signup-form";
 
 export const metadata: Metadata = {
-  title: "ورود",
+  title: "ثبت نام",
 };
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-md">
-        <LoginForm />
+        <SignupForm />
       </div>
     </div>
   );
