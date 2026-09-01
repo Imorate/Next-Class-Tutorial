f5ba146acfd37ca1345cc7c478da99f3ddd19a28
Amir Mohammad Hl <dev.imorate@gmail.com>
2026-09-02 00:35:41 +0330

    refactor: Use QueryClient as state in providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,14 +3,14 @@
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <ThemeProvider
