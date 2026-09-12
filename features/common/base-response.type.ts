a837b061a814762bf58e95576c4598a68a1f682c
Amir Mohammad Hl <dev.imorate@gmail.com>
2026-09-13 02:20:19 +0330

    fix: Make auditable properties optional

--- features/common/base-response.type.ts
+++ features/common/base-response.type.ts
@@ -1,7 +1,7 @@
 export interface BaseResponse {
   _id: string;
-  createdAt: string;
-  updatedAt: string;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export interface BaseCollectionResponse<T> {
