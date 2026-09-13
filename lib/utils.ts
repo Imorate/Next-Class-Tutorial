99b080503f2e4eb50a9c990043b3d359daa84d16
Amir Mohammad Hl <dev.imorate@gmail.com>
2026-09-14 00:09:14 +0330

    feat: Add formatPrice and getPriceWithDiscount utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat("en-US").format(price);
+}
+
+export function getPriceWithDiscount(price: number, discount: number): number {
+  if (discount > 100 || discount <= 0) {
+    throw Error("Invalid discount argument");
+  }
+  return (price * (100 - discount)) / 100;
 }
