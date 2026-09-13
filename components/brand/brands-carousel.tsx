dac778d57d6cb7570af32e6da4cd849e41aa3bef
Amir Mohammad Hl <dev.imorate@gmail.com>
2026-09-13 22:51:45 +0330

    refactor: Refactor main page carousel

--- components/brand/brands-carousel.tsx
+++ components/brand/brands-carousel.tsx
@@ -37,7 +37,7 @@ export default async function BrandsCarousel() {
                   className="object-cover transition-transform duration-300 ease-out group-hover:scale-105"
                 />
               </div>
-              <div className="px-2 py-2">
+              <div className="p-2">
                 <h3 className="truncate line-clamp-1 text-center text-xs font-medium">
                   {brand.name}
                 </h3>
